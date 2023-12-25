503df0fee558be966007717af2ef761be9fbd197
Ansh Aggarwal <anshaggarwal1007@gmail.com>
2023-12-25 12:44:35 +0530

    changes-2

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,23 @@
 const mongoose = require("mongoose");
+const DB = process.env.DATABASE;
 const mongoDB = async () => {
   await mongoose
-    .connect(
-      "mongodb+srv://anshaggarwal1007:young%401234@cluster0.d2mvc1w.mongodb.net/youngwolves_db"
-    )
+    .connect(DB)
     .then(async () => {
       console.log("connected");
-      
+
       // Fetch data from the "hoodies" collection
-      const hoodiesData = await mongoose.connection.db.collection("hoodies").find({}).toArray();
+      const hoodiesData = await mongoose.connection.db
+        .collection("hoodies")
+        .find({})
+        .toArray();
       global.hoodies = hoodiesData;
-      
+
       // Fetch data from the "hoodietype" collection
-      const hoodietypeData = await mongoose.connection.db.collection("hoodietype").find({}).toArray();
+      const hoodietypeData = await mongoose.connection.db
+        .collection("hoodietype")
+        .find({})
+        .toArray();
       global.hoodietype = hoodietypeData;
 
       //console.log("hoodies data:", global.hoodies);
